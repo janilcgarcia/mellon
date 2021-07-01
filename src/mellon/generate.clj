e9e8a3696d864256b80598e63ee980192c62ecc5
Janil C Garcia Jr <janilcgarcia@gmail.com>
2021-06-30 23:01:23 -0300

    Fixes to generate

--- src/mellon/generate.clj
+++ src/mellon/generate.clj
@@ -1,1 +1,13 @@
-(ns mellon.generate)
+(ns mellon.generate
+  (:require [clojure.string :as s]
+            [mellon.random :as m.rand]))
+
+(defn load-dict
+  [file]
+  (s/split-lines (slurp file)))
+
+(defn generate-passphrase
+  [dict len]
+  (let [byte-gen m.rand/system-byte-generator]
+    (s/join " " (repeatedly len #(m.rand/rand-element byte-gen dict)))))
+
